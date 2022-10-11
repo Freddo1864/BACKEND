d524bf2544e058a3f77bf7c7cb623541b9770b31
Freddo1864 <freddo1864@gmail.com>
2022-10-11 09:09:51 +0200

    version 1.3
    CSS en cours

--- PHP MySQL/Projet_fil_rouge/Recherche_vroomissimo/index.php
+++ PHP MySQL/Projet_fil_rouge/Recherche_vroomissimo/index.php
@@ -1,45 +1,72 @@
 <?php
-// accès a la base de données achat_voiture.
+
     $bdd = new PDO('mysql:host=localhost;dbname=achat_voiture;', 'root', '');
-// Requête pour récupérer toutes les nom_voiture de la table voitures.
-    $allcars = $bdd->query('SELECT * FROM donnees_de_base');
-// On vérifie si l'utilisateur a bien effectué sa recherche.
-    if(isset($_GET['s']) AND !empty($_GET['s'])) {
-// On stocke la recherche de l'utilisateur dans la variable ==> $recherche.
-        $recherche = htmlspecialchars($_GET['s']);
-// Requête pour vérifier dans la table le nom de la voiture entré par la recherche de l'utilisateur.
-        $allcars = $bdd->query('SELECT carrosserie, version_pays, nom_voiture FROM donnees_de_base NATURAL JOIN voitures WHERE carrosserie LIKE "%'.$recherche.'%"');
+    $query = 'SELECT nom_voiture, nom, prix, annee, kilometrage FROM voitures natural join modeles natural join prix natural join historique_vehicule';
+    $allcars = $bdd->query($query);
+
+
+    if(isset($_GET['s_marque']) OR isset($_GET['s_modele']) OR isset($_GET['s_prix'])) {
+
+        $recherche = htmlspecialchars($_GET['s_marque']);
+        $modele = htmlspecialchars($_GET['s_modele']);
+        $prix = htmlspecialchars($_GET['s_prix']);
+        $condition = array();
+
+        if(!empty($recherche)) {
+            $condition[] = 'nom_voiture LIKE "%'.$recherche.'%"';      
+        }
+
+        if(!empty($modele)) {
+            $condition[] = 'nom LIKE "%'.$modele.'%"';
+        }
+
+        if(!empty($prix)) {
+            $condition[] = 'prix LIKE "%'.$prix.'%"';
+        }
+
+    $sql = $query;
+    if (count($condition) > 0) {
+        $sql .= " WHERE " . implode(' AND ', $condition);
     }
+    $allcars = $bdd->query($sql);
+    }
+
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">    
-    <title>Rechercher des utilisateurs</title>
+    <title>Recherche de véhicules</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <h1>Bienvenue sur Vroomissimo</h1>
+    <h2>Le site de recherche de véhicules AFPA 2022</h2>
     <form method="GET">
-        <input type="search" name="s" placeholder="Rechercher">
-        <input type="submit" name="Envoyer">
+            <input type="search" name="s_marque" placeholder="Rechercher par marque">       
+            <input type="search" name="s_modele" placeholder="Rechercher par modèle">       
+            <input type="search" name="s_prix" placeholder="Rechercher par prix">       
+            <input type="submit" name="Valider">
     </form>
-
-    <section class="show_voiture">
+        <br>
+    <section class="show_car">
+        <table class="centre" id="jolie">
+            <tr><td>Marque</td> <td>Modèle</td> <td>Prix</td> <td>Année</td> <td>Kilométrage</td></tr>
         <?php
-// On vérifie qu'on a bien récupéré au moins un véhicule à partir de la requête.
+
             if($allcars->rowCount() > 0) {
-// On affiche tous les véhicules de la table voitures.
+
                 while($car = $allcars->fetch()) {
         ?>
-<!-- On affiche tous les véhicules dans un paragraphe. -->
             <table>
-                <tr><td>Carrosserie</td><td>Pays</td><td>Marque</td></tr>
                 <?php             
-                echo 
-                    $car['carrosserie']. ' ' .
-                    $car['version_pays']. ' ' .
-                    $car['nom_voiture'];
+                 echo
+                    "<tr><td>".$car['nom_voiture']."</td>
+                    <td>".$car['nom']."</td>
+                    <td>".$car['prix']."</td>
+                    <td>".$car['annee']."</td>
+                    <td>".$car['kilometrage']."</td></tr><br/>\n";
                 ?>
             </table>
         <?php            
