7de44bdceb381eca630aeb8d3a2508b2e0142ded
Freddo1864 <freddo1864@gmail.com>
2022-10-12 16:21:30 +0200

    ajout css v2
    ajout du css et image

--- PHP MySQL/Projet_fil_rouge/Recherche_vroomissimo/index.php
+++ PHP MySQL/Projet_fil_rouge/Recherche_vroomissimo/index.php
@@ -1,15 +1,16 @@
 <?php
 
     $bdd = new PDO('mysql:host=localhost;dbname=achat_voiture;', 'root', '');
-    $query = 'SELECT nom_voiture, nom, prix, annee, kilometrage FROM voitures natural join modeles natural join prix natural join historique_vehicule';
+    $query = 'SELECT nom_voiture, nom, prix, annee, kilometrage, url_image, couleur_exterieure FROM voitures natural join modeles natural join prix natural join historique_vehicule natural join images natural join infos_supplementaires';
     $allcars = $bdd->query($query);
 
 
-    if(isset($_GET['s_marque']) OR isset($_GET['s_modele']) OR isset($_GET['s_prix'])) {
+    if(isset($_GET['s_marque']) OR isset($_GET['s_modele']) OR isset($_GET['s_prix']) OR isset($GET['s_couleur'])) {
 
         $recherche = htmlspecialchars($_GET['s_marque']);
         $modele = htmlspecialchars($_GET['s_modele']);
         $prix = htmlspecialchars($_GET['s_prix']);
+        $couleur = htmlspecialchars($_GET['s_couleur']);
         $condition = array();
 
         if(!empty($recherche)) {
@@ -24,11 +25,17 @@
             $condition[] = 'prix LIKE "%'.$prix.'%"';
         }
 
+        if(!empty($couleur)) {
+            $condition[] = 'couleur_exterieure LIKE "%'.$couleur.'%"';
+        }
+
     $sql = $query;
     if (count($condition) > 0) {
         $sql .= " WHERE " . implode(' AND ', $condition);
     }
+
     $allcars = $bdd->query($sql);
+
     }
 
 ?>
@@ -46,29 +53,27 @@
     <form method="GET">
             <input type="search" name="s_marque" placeholder="Rechercher par marque">       
             <input type="search" name="s_modele" placeholder="Rechercher par modèle">       
-            <input type="search" name="s_prix" placeholder="Rechercher par prix">       
+            <input type="search" name="s_prix" placeholder="Rechercher par prix">
+            <input type="search" name="s_couleur" placeholder="Rechercher par couleur">       
             <input type="submit" name="Valider">
     </form>
         <br>
-    <section class="show_car">
+    <div class="show_car">
         <table class="centre" id="jolie">
-            <tr><td>Marque</td> <td>Modèle</td> <td>Prix</td> <td>Année</td> <td>Kilométrage</td></tr>
+            <tr id="titre"><td>Marque</td> <td>Modèle</td> <td>Prix</td> <td>Année</td> <td>Kilométrage</td> <td>Photo</td></tr>
         <?php
 
             if($allcars->rowCount() > 0) {
-
                 while($car = $allcars->fetch()) {
         ?>
-            <table>
-                <?php             
-                 echo
-                    "<tr><td>".$car['nom_voiture']."</td>
-                    <td>".$car['nom']."</td>
-                    <td>".$car['prix']."</td>
-                    <td>".$car['annee']."</td>
-                    <td>".$car['kilometrage']."</td></tr><br/>\n";
-                ?>
-            </table>
+
+            <tr><td><?= $car['nom_voiture'] ?></td>
+                    <td><?= $car['nom'] ?></td>
+                    <td><?= $car['prix'] ?> €</td>
+                    <td><?= $car['annee'] ?></td>
+                    <td><?= $car['kilometrage'] ?> kms</td>
+                    <td class="image"><img src="<?= $car['url_image'] ?>"></td>
+                    </tr>
         <?php            
                 }
             } else {
@@ -78,6 +83,6 @@
         <?php
             }
         ?>
-    </section>
+    </div>
 </body>
 </html>
