543d27f4146b0b31fdc012647f9203c0706646aa
Freddo1864 <freddo1864@gmail.com>
2022-10-13 15:11:26 +0200

    ajout vehicule bdd et css
    ajout de vehicule dans bdd
    modif vehicule dans bdd
    ajout CSS

--- PHP MySQL/Projet_fil_rouge/Recherche_vroomissimo/BDD_Fil_rouge_v3.sql
+++ PHP MySQL/Projet_fil_rouge/Recherche_vroomissimo/BDD_Fil_rouge_v3.sql
@@ -99,7 +99,7 @@ CREATE TABLE `historique_vehicule` (
 
 LOCK TABLES `historique_vehicule` WRITE;
 /*!40000 ALTER TABLE `historique_vehicule` DISABLE KEYS */;
-INSERT INTO `historique_vehicule` VALUES (1,231333,'2000',2000,3,'oui','non'),(2,153120,'1999',1997,2,'oui','oui'),(3,22045,'2012',2011,1,'non','oui'),(4,178000,'1999',1998,3,'oui','oui'),(5,101150,'2011',2011,2,'oui','non'),(6,220000,'2017',2016,1,'oui','non'),(7,105214,'2016',2014,3,'non','oui'),(8,171000,'2018',2017,2,'oui','oui'),(9,26000,'2020',2020,1,'oui','non'),(10,193450,'2019',2016,2,'oui','non');
+INSERT INTO `historique_vehicule` VALUES (1,231333,'2000',2000,3,'oui','non'),(2,153120,'1999',1997,2,'oui','oui'),(3,22045,'2012',2011,1,'non','oui'),(4,178000,'1999',1998,3,'oui','oui'),(5,101150,'2011',2011,2,'oui','non'),(6,220000,'2017',2016,1,'oui','non'),(7,105214,'2016',2014,3,'non','oui'),(8,171000,'2018',2017,2,'oui','oui'),(9,26000,'2020',2020,1,'oui','non'),(10,193450,'2019',2016,2,'oui','non'),(11,34120,'2020',2020,1,'oui','non'),(12,12050,'2022',2021,1,'oui','non');
 /*!40000 ALTER TABLE `historique_vehicule` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -114,8 +114,11 @@ CREATE TABLE `images` (
   `id_image` int(11) NOT NULL AUTO_INCREMENT,
   `nom_image` varchar(100) DEFAULT NULL,
   `url_image` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id_image`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id_voiture` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_image`),
+  KEY `id_voiture` (`id_voiture`),
+  CONSTRAINT `images_ibfk_1` FOREIGN KEY (`id_voiture`) REFERENCES `voitures` (`id_voiture`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -124,6 +127,7 @@ CREATE TABLE `images` (
 
 LOCK TABLES `images` WRITE;
 /*!40000 ALTER TABLE `images` DISABLE KEYS */;
+INSERT INTO `images` VALUES (1,'volkswagen_sirocco','https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/VW_Scirocco_III_R_Risingblue.JPG/270px-VW_Scirocco_III_R_Risingblue.JPG',1),(2,'audi_a3','https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Audi_A3_Sportback_front_20080809.jpg/294px-Audi_A3_Sportback_front_20080809.jpg',2),(3,'volkswagen_passat','https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/6th-Volkswagen-Passat.jpg/280px-6th-Volkswagen-Passat.jpg',5),(4,'seat_toledo','https://upload.wikimedia.org/wikipedia/commons/5/5a/Seat_Toledo_1.4_TSI_Style_Salsa_%28IV%29_%E2%80%93_Frontansicht%2C_1._September_2013%2C_M%C3%BCnster.jpg',4),(5,'volkswagen_tiguan','https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Volkswagen_Tiguan_Allspace_FL_IMG_5741.jpg/220px-Volkswagen_Tiguan_Allspace_FL_IMG_5741.jpg',3),(6,'volkswagen_lupo','https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Car_VW_Lupo_2_wikipedia.JPG/1024px-Car_VW_Lupo_2_wikipedia.JPG',6),(7,'seat_leon','https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/SEAT_Leon_Mk4_IMG_3445.jpg/1200px-SEAT_Leon_Mk4_IMG_3445.jpg',7),(8,'volkswagen_golf','https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/VW_Golf_I_Facelift_front_20081209.jpg/289px-VW_Golf_I_Facelift_front_20081209.jpg',8),(9,'volkswagen_polo','https://upload.wikimedia.org/wikipedia/commons/9/91/VW_Polo_beats_%28VI%29_%E2%80%93_f_03032019_%28exposure_adjusted%29.jpg',9),(10,'audi_a1','https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Audi_A1_35_TFSI_S_line_%28II%29_%E2%80%93_f_22092019.jpg/640px-Audi_A1_35_TFSI_S_line_%28II%29_%E2%80%93_f_22092019.jpg',10),(11,'seat_ibiza','https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Seat_Ibiza_FR_%281%29_%E2%80%93_IAA_2017_%E2%80%93_by_Nicolas_V%C3%B6lcker.jpg/2560px-Seat_Ibiza_FR_%281%29_%E2%80%93_IAA_2017_%E2%80%93_by_Nicolas_V%C3%B6lcker.jpg',11),(12,'seat_ateca','https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Seat_IBe_%28front_quarter%29.jpg/220px-Seat_IBe_%28front_quarter%29.jpg',12),(13,'audi_q3','https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Audi_RS_Q3_F3_IMG_2071.jpg/1280px-Audi_RS_Q3_F3_IMG_2071.jpg',13),(14,'audi_tt','https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Audi_TTS_Glutorange.JPG/280px-Audi_TTS_Glutorange.JPG',14);
 /*!40000 ALTER TABLE `images` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -150,7 +154,7 @@ CREATE TABLE `infos_supplementaires` (
 
 LOCK TABLES `infos_supplementaires` WRITE;
 /*!40000 ALTER TABLE `infos_supplementaires` DISABLE KEYS */;
-INSERT INTO `infos_supplementaires` VALUES (1,'noir','mat','beige','tissu'),(2,'gris','metallise','gris','tissu'),(3,'gris','metallise','noir','cuir'),(4,'rouge','mat','noir','tissu'),(5,'noir','mat','gris','tissu'),(6,'bleu','metallise','gris','cuir'),(7,'gris','mat','blanc','cuir'),(8,'rouge','metallise','beige','tissu'),(9,'noir','metallise','rouge','cuir'),(10,'blanc','mat','blanc','tissu');
+INSERT INTO `infos_supplementaires` VALUES (1,'noire','mat','beige','tissu'),(2,'gris','metallise','gris','tissu'),(3,'gris','metallise','noir','cuir'),(4,'rouge','mat','noir','tissu'),(5,'noire','mat','gris','tissu'),(6,'bleu','metallise','gris','cuir'),(7,'gris','mat','blanc','cuir'),(8,'rouge','metallise','beige','tissu'),(9,'noire','metallise','rouge','cuir'),(10,'blanc','mat','blanc','tissu'),(11,'orange','mat','noir','tissu'),(12,'vert','mat','noir','tissu'),(13,'marron','mat','beige','tissu'),(14,'jaune','metallise','blanc','cuir');
 /*!40000 ALTER TABLE `infos_supplementaires` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -174,7 +178,7 @@ CREATE TABLE `modeles` (
 
 LOCK TABLES `modeles` WRITE;
 /*!40000 ALTER TABLE `modeles` DISABLE KEYS */;
-INSERT INTO `modeles` VALUES (1,'golf'),(2,'a3'),(3,'passat'),(4,'toledo'),(5,'passat'),(6,'lupo'),(7,'leon'),(8,'golf'),(9,'polo'),(10,'a1'),(11,'ibiza'),(12,'ateca'),(13,'Q3'),(14,'TT');
+INSERT INTO `modeles` VALUES (1,'sirocco'),(2,'a3'),(3,'passat'),(4,'toledo'),(5,'tiguan'),(6,'lupo'),(7,'leon'),(8,'golf'),(9,'polo'),(10,'a1'),(11,'ibiza'),(12,'ateca'),(13,'Q3'),(14,'TT');
 /*!40000 ALTER TABLE `modeles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -198,7 +202,7 @@ CREATE TABLE `prix` (
 
 LOCK TABLES `prix` WRITE;
 /*!40000 ALTER TABLE `prix` DISABLE KEYS */;
-INSERT INTO `prix` VALUES (1,24900),(2,1290),(3,14150),(4,4000),(5,2900),(6,10500),(7,6500),(8,8000),(9,4500),(10,990);
+INSERT INTO `prix` VALUES (1,24900),(2,1290),(3,14150),(4,4000),(5,2900),(6,10500),(7,6500),(8,8000),(9,4500),(10,990),(11,35000),(12,28750);
 /*!40000 ALTER TABLE `prix` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -222,7 +226,7 @@ CREATE TABLE `types` (
 
 LOCK TABLES `types` WRITE;
 /*!40000 ALTER TABLE `types` DISABLE KEYS */;
-INSERT INTO `types` VALUES (1,'GLI Sportline'),(2,'1.6i Confort'),(3,'V6 4Motion'),(4,'1.8 GL'),(5,'2.5 V6 TDI Confort A'),(6,'2.8i V6 4Motion'),(7,'1.6'),(8,'V6 4Motion'),(9,'1.6 gti'),(10,'1.9 TDI GTI');
+INSERT INTO `types` VALUES (1,'GLI Sportline'),(2,'1.6i Confort'),(3,'V6 4Motion'),(4,'1.8 GL'),(5,'2.5 V6 TDI Confort A'),(6,'2.8i V6 4Motion'),(7,'1.6'),(8,'V8 4Motion'),(9,'1.6 gti'),(10,'1.9 TDI GTI');
 /*!40000 ALTER TABLE `types` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -274,7 +278,6 @@ CREATE TABLE `voitures` (
   `id_prix` int(11) NOT NULL,
   `id_type` int(11) NOT NULL,
   `id_modele` int(11) NOT NULL,
-  `id_image` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_voiture`),
   KEY `id_vendeur` (`id_vendeur`),
   KEY `id_donnee` (`id_donnee`),
@@ -284,7 +287,6 @@ CREATE TABLE `voitures` (
   KEY `id_prix` (`id_prix`),
   KEY `id_type` (`id_type`),
   KEY `id_modele` (`id_modele`),
-  KEY `id_image` (`id_image`),
   CONSTRAINT `voitures_ibfk_1` FOREIGN KEY (`id_vendeur`) REFERENCES `vendeurs` (`id_vendeur`),
   CONSTRAINT `voitures_ibfk_2` FOREIGN KEY (`id_donnee`) REFERENCES `donnees_de_base` (`id_donnee`),
   CONSTRAINT `voitures_ibfk_3` FOREIGN KEY (`id_historique`) REFERENCES `historique_vehicule` (`id_historique`),
@@ -292,8 +294,7 @@ CREATE TABLE `voitures` (
   CONSTRAINT `voitures_ibfk_5` FOREIGN KEY (`id_info`) REFERENCES `infos_supplementaires` (`id_info`),
   CONSTRAINT `voitures_ibfk_6` FOREIGN KEY (`id_prix`) REFERENCES `prix` (`id_prix`),
   CONSTRAINT `voitures_ibfk_7` FOREIGN KEY (`id_type`) REFERENCES `types` (`id_type`),
-  CONSTRAINT `voitures_ibfk_8` FOREIGN KEY (`id_modele`) REFERENCES `modeles` (`id_modele`),
-  CONSTRAINT `voitures_ibfk_9` FOREIGN KEY (`id_image`) REFERENCES `images` (`id_image`)
+  CONSTRAINT `voitures_ibfk_8` FOREIGN KEY (`id_modele`) REFERENCES `modeles` (`id_modele`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -303,7 +304,7 @@ CREATE TABLE `voitures` (
 
 LOCK TABLES `voitures` WRITE;
 /*!40000 ALTER TABLE `voitures` DISABLE KEYS */;
-INSERT INTO `voitures` VALUES (1,'volkswagen',1,5,4,4,2,5,3,1,NULL),(2,'audi',5,6,8,6,5,1,8,10,NULL),(3,'volkswagen',7,1,10,1,7,8,4,3,NULL),(4,'seat',3,2,3,2,3,6,7,4,NULL),(5,'audi',10,8,6,9,1,7,10,2,NULL),(6,'seat',2,3,9,3,10,3,2,7,NULL),(7,'volkswagen',9,7,2,5,4,2,7,9,NULL),(8,'audi',6,4,1,7,6,4,1,13,NULL),(9,'seat',4,9,5,8,8,10,5,11,NULL),(10,'volkswagen',8,10,7,10,9,9,6,6,NULL),(11,'audi',10,5,4,5,4,1,5,14,NULL),(12,'seat',4,7,3,3,6,6,8,12,NULL);
+INSERT INTO `voitures` VALUES (1,'volkswagen',1,5,4,4,6,5,3,1),(2,'audi',5,6,8,6,2,1,8,2),(3,'volkswagen',7,1,10,1,9,8,4,5),(4,'seat',3,2,3,2,5,6,7,4),(5,'volkswagen',10,8,6,9,3,7,10,3),(6,'volkswagen',2,3,9,3,12,3,2,6),(7,'seat',9,7,2,5,8,2,7,7),(8,'volkswagen',6,4,1,7,13,4,1,8),(9,'volkswagen',4,9,5,8,2,10,5,9),(10,'audi',8,10,7,10,14,9,6,10),(11,'seat',10,5,4,5,4,1,5,11),(12,'seat',4,7,3,3,8,6,8,12),(13,'audi',9,9,11,3,4,12,6,13),(14,'audi',5,1,12,6,11,11,8,14);
 /*!40000 ALTER TABLE `voitures` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -320,4 +321,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-11 13:53:39
+-- Dump completed on 2022-10-13 15:08:03
--- PHP MySQL/Projet_fil_rouge/Recherche_vroomissimo/index.php
+++ PHP MySQL/Projet_fil_rouge/Recherche_vroomissimo/index.php
@@ -1,10 +1,16 @@
 <?php
-
+// Accès à la base de données avec l'objet PDO.
     $bdd = new PDO('mysql:host=localhost;dbname=achat_voiture;', 'root', '');
+
+// Requête pour la recherche dans la base de données.
     $query = 'SELECT nom_voiture, nom, prix, annee, kilometrage, url_image, couleur_exterieure FROM voitures natural join modeles natural join prix natural join historique_vehicule natural join images natural join infos_supplementaires';
+
+// La fonction query exécute une requête sql en appelant une seule fonction    
     $allcars = $bdd->query($query);
 
 
+// La fonction isset vérifie si la variable est définie.
+// La variable superglobable $_GET contient les données envoyées en paramètres dans l'URL.
     if(isset($_GET['s_marque']) OR isset($_GET['s_modele']) OR isset($_GET['s_prix']) OR isset($GET['s_couleur'])) {
 
         $recherche = htmlspecialchars($_GET['s_marque']);
@@ -12,7 +18,7 @@
         $prix = htmlspecialchars($_GET['s_prix']);
         $couleur = htmlspecialchars($_GET['s_couleur']);
         $condition = array();
-
+// La fonction empty vérifie si la variable est définie et si celle-ci est égale à NULL, "", 0, etc....
         if(!empty($recherche)) {
             $condition[] = 'nom_voiture LIKE "%'.$recherche.'%"';      
         }
@@ -31,7 +37,7 @@
 
     $sql = $query;
     if (count($condition) > 0) {
-        $sql .= " WHERE " . implode(' AND ', $condition);
+        $sql .= " WHERE " . implode(' AND ', $condition) . ' ORDER BY prix DESC';
     }
 
     $allcars = $bdd->query($sql);
@@ -46,34 +52,44 @@
     <meta charset="UTF-8">    
     <title>Recherche de véhicules</title>
     <link rel="stylesheet" href="style.css">
-</head>
+    </head>
 <body>
+    <div id="demotext">
     <h1>Bienvenue sur Vroomissimo</h1>
     <h2>Le site de recherche de véhicules AFPA 2022</h2>
+    
     <form method="GET">
-            <input type="search" name="s_marque" placeholder="Rechercher par marque">       
-            <input type="search" name="s_modele" placeholder="Rechercher par modèle">       
-            <input type="search" name="s_prix" placeholder="Rechercher par prix">
-            <input type="search" name="s_couleur" placeholder="Rechercher par couleur">       
+        <label for="marque" >Marque : </label>
+            <input type="search" name="s_marque">
+        <label for="marque" >Modèle : </label>   
+            <input type="search" name="s_modele">
+        <label for="marque" >Prix : </label>
+            <input type="search" name="s_prix">
+        <label for="marque" >Couleur : </label>
+            <input type="search" name="s_couleur">
+            <br>      
             <input type="submit" name="Valider">
     </form>
+    </div>
         <br>
     <div class="show_car">
         <table class="centre" id="jolie">
-            <tr id="titre"><td>Marque</td> <td>Modèle</td> <td>Prix</td> <td>Année</td> <td>Kilométrage</td> <td>Photo</td></tr>
+            <tr id="titre"><td>Photo</td><td>Marque</td> <td>Modèle</td> <td>Prix</td> <td>Année</td> <td>Kilométrage</td> </tr>
         <?php
 
             if($allcars->rowCount() > 0) {
                 while($car = $allcars->fetch()) {
-        ?>
-
-            <tr><td><?= $car['nom_voiture'] ?></td>
+                    ?>
+            
+                <tr>
+                    <td class="image"><img src="<?= $car['url_image'] ?>"></td>                    
+                    <td><?= $car['nom_voiture'] ?></td>
                     <td><?= $car['nom'] ?></td>
                     <td><?= $car['prix'] ?> €</td>
                     <td><?= $car['annee'] ?></td>
                     <td><?= $car['kilometrage'] ?> kms</td>
-                    <td class="image"><img src="<?= $car['url_image'] ?>"></td>
-                    </tr>
+                </tr>
+
         <?php            
                 }
             } else {
